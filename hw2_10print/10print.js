42bc2e26d6f41e35bdac822d677a7180e1abc3d2
Shayna Kaushal <shaynakaushal@gmail.com>
2022-02-21 00:19:20 -0600

    i like it

--- hw2_10print/10print.js
+++ hw2_10print/10print.js
@@ -1,67 +1,58 @@
 const sw = process.stdout.columns
 
-// const all_types = ['blocks', 'crosses', 'lines', 'corners', 'hollow_corners', 'shades', 'semicircles']
-
-const blocks = [ '█', '▊', '▋', '▌', '▍', '▎', '▏']
-const crosses = ['╳', '╱', '╲']
-const lines = ['┃', '│', '┇', '┆', '┋', '┊']
-const corners = ['┓', '┑', '┕', '┗', '┿', '├', '┝', '┥', '┫']
-const hollow_corners = ['═', '║', '╬', '╞', '╟', '╢', '╡']
-const shades = ['▓', '▒', '░']
-const semicircles = ['◠', '◡']
-const triangles = ['◢','◣','◤','◥','◼']
-// ['', '', '', '', '', '', '', '', '', '']
+/** 
+ * Pick your own symbols! 
+ * 
+ * Select any array to add to 'types' array
+ * 
+ * const blocks = [ '█', '▊', '▋', '▌', '▍', '▎', '▏']
+ * const crosses = ['╳', '╱', '╲']
+ * const lines = ['┃', '│', '┇', '┆', '┋', '┊']
+ * const corners = ['┓', '┑', '┕', '┗', '┿', '├', '┝', '┥', '┫']
+ * const hollow_corners = ['═', '║', '╬', '╞', '╟', '╢', '╡']
+ * const shades = ['▓', '▒', '░']
+ * const semicircles = ['◠', '◡']
+ * const triangles = ['◢','◣','◤','◥','◼']
+ * 
+ * extra: ['', '', '', '', '', '', '', '', '', '']
+ * */
 
 const types = [
-    // [ '█', '▊', '▋', '▌', '▍', '▎', '▏'],
     ['╳', '╱', '╲'],
     ['┃', '│', '┇', '┆', '┋', '┊'],
     ['◢','◣','◤','◥','◼'],
     ['┓', '┑', '┕', '┗', '┿', '├', '┝', '┥', '┫'],
+    ['▓', '▒', '░'],
 ]
 
-// const d_fcolors = ['\x1b[30m', '\x1b[31m', '\x1b[32m', '\x1b[33m', '\x1b[34m', '\x1b[35m', '\x1b[36m', '\x1b[37m', '\x1b[38m']
-// const b_fcolors = ['\x1b[90m', '\x1b[91m', '\x1b[92m', '\x1b[93m', '\x1b[94m', '\x1b[95m', '\x1b[96m', '\x1b[97m', '\x1b[98m']
-const b_fcolors = ['\x1b[90m', '\x1b[91m',  '\x1b[94m', '\x1b[95m', '\x1b[96m', '\x1b[97m']
-// const b_fcolors = ['\x1b[91m',  '\x1b[94m', '\x1b[95m', '\x1b[96m', '\x1b[97m']
+const b_fcols = ['\x1b[90m', '\x1b[91m', '\x1b[94m', '\x1b[95m', '\x1b[96m', '\x1b[97m']
+const b_bcols = ['\x1b[40m', '\x1b[41m', '\x1b[42m', '\x1b[43m', '\x1b[44m', '\x1b[45m', '\x1b[46m', '\x1b[47m']
+//Order:         BLACK       RED         GREEN       YELLOW      BLUE        MAGENTA     CYAN        WHITE
 
-const b_bcolors = ['\x1b[40m', '\x1b[41m', '\x1b[42m', '\x1b[43m', '\x1b[44m', '\x1b[45m', '\x1b[46m', '\x1b[47m']
-//  BLACK   RED    GREEN   YELLOW   BLUE    MAGENTA     CYAN    WHITE
-// '\x1b[92m', '\x1b[93m',
-// const colors = [A569BD, F5B7B1, E566E3, CAAAE1, A000D3]
-// '\x1b[92m', '\x1b[93m',
-
-let w1 = 0
+let w = 0
 let flag = 4
 
+function get_random (x){ return Math.floor(Math.random() * x) }
+
 function draw () {
-    setTimeout(draw, 1000/5)
-    if (w1 > sw/2 -1 || w1 < 0){
-        flag *= -1
-    }
-    w1 += flag
-    let choose_type = Math.floor(Math.random() * types.length)
+    setTimeout(draw, 1000/8)
 
-    // let col_rand = Math.floor(Math.random() * colors.length);
+    if (w > sw/2 -1 || w < 0){ flag *= -1 }
+    w += flag
+    
+    let type = get_random(types.length)
+    let item = get_random(types[type].length)
+    let fcolor = get_random(b_fcols.length)
 
-    let output = ''
-    for (let i = 0; i < w1-1; i++) {
-        let item = Math.floor(Math.random() * types[choose_type].length)
-        let fcolor = Math.floor(Math.random() * b_fcolors.length)
-        // let tmp_col = colors[col_rand]
-        // output += b_fcolors[fcolor] + types[choose_type][item]
-        output += b_fcolors[fcolor] + types[choose_type][item]
+    let output = '', output2 = ''
+    for (let i = 0; i < w-1; i++) {
+        output += b_fcols[fcolor] + types[type][item]
     }
 
-    let output2 = ''
     for (let i = sw-1; i > sw/2; i--){
-        let item = Math.floor(Math.random() * types[choose_type].length)
-        let fcolor = Math.floor(Math.random() * b_fcolors.length)
-        output2 += b_fcolors[fcolor] + types[choose_type][item]
-        output2.padStart(sw, '0')
+        output2 += b_fcols[fcolor] + types[type][item]
     }
     console.log('\x1b[0m'+output+output2)
-
 }
 
 draw()
