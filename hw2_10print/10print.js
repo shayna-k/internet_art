d69cc3825aba359781507411b9358fae5ef86b82
Shayna Kaushal <shaynakaushal@gmail.com>
2022-02-21 00:00:43 -0600

    tiny chnages

--- hw2_10print/10print.js
+++ hw2_10print/10print.js
@@ -22,26 +22,34 @@ const types = [
 
 // const d_fcolors = ['\x1b[30m', '\x1b[31m', '\x1b[32m', '\x1b[33m', '\x1b[34m', '\x1b[35m', '\x1b[36m', '\x1b[37m', '\x1b[38m']
 // const b_fcolors = ['\x1b[90m', '\x1b[91m', '\x1b[92m', '\x1b[93m', '\x1b[94m', '\x1b[95m', '\x1b[96m', '\x1b[97m', '\x1b[98m']
-const b_fcolors = ['\x1b[91m', '\x1b[92m', '\x1b[93m', '\x1b[94m', '\x1b[95m', '\x1b[96m', '\x1b[97m']
-const b_bcolors = ['\x1b[40m', '\x1b[41m', '\x1b[42m', '\x1b[43m', '\x1b[44m', '\x1b[45m', '\x1b[46m', '\x1b[47m', '\x1b[48m']
-//  BLACK   RED    GREEN   YELLOW   BLUE    MAGENTA     CYAN    WHITE
+const b_fcolors = ['\x1b[90m', '\x1b[91m',  '\x1b[94m', '\x1b[95m', '\x1b[96m', '\x1b[97m']
+// const b_fcolors = ['\x1b[91m',  '\x1b[94m', '\x1b[95m', '\x1b[96m', '\x1b[97m']
 
+const b_bcolors = ['\x1b[40m', '\x1b[41m', '\x1b[42m', '\x1b[43m', '\x1b[44m', '\x1b[45m', '\x1b[46m', '\x1b[47m']
+//  BLACK   RED    GREEN   YELLOW   BLUE    MAGENTA     CYAN    WHITE
+// '\x1b[92m', '\x1b[93m',
+// const colors = [A569BD, F5B7B1, E566E3, CAAAE1, A000D3]
+// '\x1b[92m', '\x1b[93m',
 
 let w1 = 0
 let flag = 4
 
 function draw () {
-    setTimeout(draw, 1000/10)
+    setTimeout(draw, 1000/5)
     if (w1 > sw/2 -1 || w1 < 0){
         flag *= -1
     }
     w1 += flag
     let choose_type = Math.floor(Math.random() * types.length)
 
+    // let col_rand = Math.floor(Math.random() * colors.length);
+
     let output = ''
     for (let i = 0; i < w1-1; i++) {
         let item = Math.floor(Math.random() * types[choose_type].length)
         let fcolor = Math.floor(Math.random() * b_fcolors.length)
+        // let tmp_col = colors[col_rand]
+        // output += b_fcolors[fcolor] + types[choose_type][item]
         output += b_fcolors[fcolor] + types[choose_type][item]
     }
 
